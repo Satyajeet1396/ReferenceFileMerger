a21ac80abb3654ae2ca34a4ad0e91e492bf37323
Satyajeet1396 <36727765+Satyajeet1396@users.noreply.github.com>
2024-10-29 16:16:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,3 +70,13 @@ else:
 
 st.info("Created by Dr. Satyajeet Patil")
 st.info("For more cool apps like this visit: https://patilsatyajeet.wixsite.com/home/python")
+
+
+# Display custom "Buy Me a Coffee" button
+bmc_button = """
+<div align="center">
+    <a href="https://www.buymeacoffee.com/researcher13" target="_blank">
+        <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Support our Research" style="height: 50px; width: 217px;">
+    </a>
+</div>
+"""
